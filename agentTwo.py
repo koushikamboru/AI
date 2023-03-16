e0031d13b9e230bb777929bb486a05da465048bf
Koushik Amboru <80423048+koushikamboru@users.noreply.github.com>
2023-03-16 15:20:10 +0530

    Update agentTwo.py
    Just added parathesis to print statement

--- agentTwo.py
+++ agentTwo.py
@@ -14,80 +14,80 @@ class Environment(object):
 
 class SimpleReflexVacuumAgent(Environment):
     def __init__(self, Environment):
-        print Environment.locationCondition
+        print (Environment.locationCondition)
         # Instantiate performance measurement
         Score = 0
         # place vacuum at random location
         vacuumLocation = random.randint(0, 1)
         # if vacuum at A
         if vacuumLocation == 0:
-            print "Vacuum is randomly placed at Location A"
+            print ("Vacuum is randomly placed at Location A")
             # and A is Dirty
             if Environment.locationCondition['A'] == 1:
-                print "Location A is Dirty. "
+                print ("Location A is Dirty. ")
                 # suck and mark clean
                 Environment.locationCondition['A'] = 0;
                 Score += 1
-                print "Location A has been Cleaned. :D"
+                print ("Location A has been Cleaned. :D")
 
                 # if B is Dirty
                 if Environment.locationCondition['B'] == 1:
-                    print "Location B is Dirty."
+                    print ("Location B is Dirty.")
                     # move to B
-                    print "Moving to Location B..."
+                    print ("Moving to Location B...")
                     Score -= 1
                     # suck and mark clean
                     Environment.locationCondition['B'] = 0;
                     Score += 1
-                    print "Location B has been Cleaned :D."
+                    print ("Location B has been Cleaned :D.")
             else:
 
                 # if B is Dirty
                 if Environment.locationCondition['B'] == 1:
-                    print "Location B is Dirty."
+                    print ("Location B is Dirty.")
                     # move to B
                     Score -= 1
-                    print "Moving to Location B..."
+                    print ("Moving to Location B...")
                     # suck and mark clean
                     Environment.locationCondition['B'] = 0;
                     Score += 1
-                    print "Location B has been Cleaned. :D"
+                    print ("Location B has been Cleaned. :D")
 
         elif vacuumLocation == 1:
-            print "Vacuum is randomly placed at Location B. "
+            print ("Vacuum is randomly placed at Location B. ")
             # and B is Dirty
             if Environment.locationCondition['B'] == 1:
-                print "Location B is Dirty"
+                print ("Location B is Dirty")
                 # suck and mark clean
                 Environment.locationCondition['B'] = 0;
                 Score += 1
-                print "Location B has been Cleaned"
+                print ("Location B has been Cleaned")
 
                 # if A is Dirty
                 if Environment.locationCondition['A'] == 1:
-                    print "Location A is Dirty"
+                    print ("Location A is Dirty")
                     # move to A
                     Score -= 1
-                    print "Moving to Location A"
+                    print ("Moving to Location A")
                     # suck and mark clean
                     Environment.locationCondition['A'] = 0;
                     Score += 1
-                    print "Location A has been Cleaned"
+                    print ("Location A has been Cleaned")
             else:
 
                 # if A is Dirty
                 if Environment.locationCondition['A'] == 1:
-                    print "Location A is Dirty"
+                    print ("Location A is Dirty")
                     # move to A
-                    print "Moving to Location A"
+                    print ("Moving to Location A")
                     Score -= 1
                     # suck and mark clean
                     Environment.locationCondition['A'] = 0;
                     Score += 1
-                    print "Location A has been Cleaned"
+                    print ("Location A has been Cleaned")
         # done cleaning
-        print Environment.locationCondition
-        print "Performance Measurement: " + str(Score)
+        print (Environment.locationCondition)
+        print ("Performance Measurement: " + str(Score))
 
 
 theEnvironment = Environment()
